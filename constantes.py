8c2820ad28bf78f4aca19b21127ee1ac0cb6309e
dev-DaviGuerra <davimguerra@gmail.com>
2025-11-12 17:52:24 -0300

    First commit

---
+++ constantes.py
@@ -0,0 +1,35 @@
+import os
+import logging
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+TICKER = "AAPL"
+EMPRESA = "Apple Inc."
+
+try:
+    DB_STRING = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    db_engine = create_engine(DB_STRING)
+    print("Engine do DB criada com sucesso.")
+except Exception as e:
+    print(f"Erro ao criar engine do DB: {e}")
+    db_engine = None
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("stockintel_pipeline.log"),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
